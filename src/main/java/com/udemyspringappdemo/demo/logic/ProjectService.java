0f6f51aa080e6a9dc21a0e9a00272a20811a9fa1
Bartek <bartekgrzesiuk@gmail.com>
2021-03-26 23:40:56 +0100

    services added

---
+++ src/main/java/com/udemyspringappdemo/demo/logic/ProjectService.java
@@ -0,0 +1,61 @@
+package com.udemyspringappdemo.demo.logic;
+
+import com.udemyspringappdemo.demo.model.*;
+import com.udemyspringappdemo.demo.model.projection.GroupReadModel;
+import com.udemyspringappdemo.demo.model.projection.GroupWriteModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectService {
+
+    private ProjectRepository repository;
+    private TaskGroupRepository taskGroupRepository;
+    private TaskConfigurationProperties config;
+
+
+    public ProjectService(ProjectRepository repository,
+                          TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
+        this.repository = repository;
+        this.taskGroupRepository = taskGroupRepository;
+        this.config = config;
+    }
+
+    public List<Project> readAll() {
+
+        return repository.findAll();
+    }
+
+    public Project save(Project toSave) {
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(int projectId, LocalDateTime deadLine) {
+
+        if (!config.getTemplate().isAllowMultipleTasks() &&
+                taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+            throw new IllegalStateException("only one undone group from project is allowed");
+        }
+
+      TaskGroup result =   repository.findById(projectId)
+                .map(project -> {
+            var targetGroup = new TaskGroup();
+                    targetGroup.setDescription(project.getDescription());
+                    targetGroup.setTasks(project.getSteps()
+                    .stream()
+                    .map(projectStep -> new Task(
+                            project.getDescription(), deadLine.plusDays(projectStep.getDaysToDeadLine())))
+                    .collect(Collectors.toSet()));
+
+
+            return targetGroup;
+
+        }).orElseThrow(()->new IllegalStateException("Project with given id not found"));
+        return new GroupReadModel(result);
+    }
+}
+
---
+++ src/test/java/com/udemyspringappdemo/demo/logic/ProjectServiceTest.java
@@ -0,0 +1,49 @@
+package com.udemyspringappdemo.demo.logic;
+
+import com.udemyspringappdemo.demo.model.TaskConfigurationProperties;
+import com.udemyspringappdemo.demo.model.TaskGroupRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE_TIME;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+class ProjectServiceTest {
+
+    @Test
+    @DisplayName("should throw IllegalStateException when configured to allow just one group and the other undone group exist")
+    void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwsIllegalStateException() {
+
+        //given
+        var mockGroupRepository = mock(TaskGroupRepository.class);
+        when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(true);
+        //and
+        var mockTemoplate =mock(TaskConfigurationProperties.Template.class);
+        when(mockTemoplate.isAllowMultipleTasks()).thenReturn(false);
+        //and
+        var mockConfig = mock(TaskConfigurationProperties.class);
+        when(mockConfig.getTemplate()).thenReturn(mockTemoplate);
+        //system under test
+        var toTest = new ProjectService(null,mockGroupRepository,mockConfig);
+        //when
+        var exception = catchThrowable(()->toTest.createGroup(0,LocalDateTime.now()));
+        //then
+
+        assertThat(exception)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("one undone group");
+        }
+
+
+  /*
+        assertThatExceptionOfType(IllegalStateException.class).isThrownBy(()->
+           toTest.createGroup(0,LocalDateTime.now())).isInstanceOf(IllegalStateException.class);
+         */
+
+
+    }
