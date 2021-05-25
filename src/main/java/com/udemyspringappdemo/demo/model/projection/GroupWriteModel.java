551bd69ebdf8b791f77235951cee0927e5f732ea
Bartek <bartekgrzesiuk@gmail.com>
2021-05-26 01:23:36 +0200

    few changes in ProjectService and GroupTask write and read classes

--- src/main/java/com/udemyspringappdemo/demo/model/projection/GroupWriteModel.java
+++ src/main/java/com/udemyspringappdemo/demo/model/projection/GroupWriteModel.java
@@ -1,39 +1,27 @@
 package com.udemyspringappdemo.demo.model.projection;
 
 
-
 import com.udemyspringappdemo.demo.model.TaskGroup;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GroupWriteModel {
 
+    @Getter
+    @Setter
     private String description;
+    @Getter
+    @Setter
     private Set<GroupTaskWriteModel> tasks;
-
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<GroupTaskWriteModel> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(Set<GroupTaskWriteModel> tasks) {
-        this.tasks = tasks;
-    }
 
     public TaskGroup toGroup() {
         var result = new TaskGroup();
         result.setDescription(description);
         result.setTasks(tasks.stream()
-                .map(GroupTaskWriteModel::toTask)
+                .map(source -> source.toTask(result))
                 .collect(Collectors.toSet()));
         return result;
     }
